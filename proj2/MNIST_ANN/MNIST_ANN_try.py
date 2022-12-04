79c2150fe05cb9875f711f83ee32f0ff3eb02ad8
DD <1246422498@qq.com>
2022-12-04 16:13:50 +0800

    ANN part completed

--- proj2/MNIST_ANN/MNIST_ANN_try.py
+++ proj2/MNIST_ANN/MNIST_ANN_try.py
@@ -1,147 +1,128 @@
-from __future__ import print_function
-import argparse
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torchvision import datasets, transforms
-from torch.optim.lr_scheduler import StepLR
+from torchvision.datasets import MNIST
+from torchvision.transforms import Compose, ToTensor, Normalize
+from torch.utils.data import DataLoader
 
 
-class Net(nn.Module):
+BATCH_SIZE = 512
+# 一次输入量
+EPOCHS = 2
+# 总共训练批次
+gpu_abailable = torch.cuda.is_available()
+print(gpu_abailable)
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# 判断是否使用GPU
+
+
+# 拿数据
+def get_dataloader(train, batch_size=BATCH_SIZE):
+    transform_fn = Compose([
+        ToTensor(),
+        Normalize(mean=(0.1307,), std=(0.3081,))
+    ])  # mean和std的形状与通道数相同
+
+    dataset = MNIST(download=True, root='./data',
+                    train=train, transform=transform_fn)
+    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    return data_loader
+
+
+train_loader = get_dataloader(train=True)
+test_loader = get_dataloader(train=False)
+
+
+# Create ANN Model
+class ANNModel(nn.Module):
+
     def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.dropout1 = nn.Dropout(0.25)
-        self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
+        super(ANNModel, self).__init__()
 
-    def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x, 2)
-        x = self.dropout1(x)
-        x = torch.flatten(x, 1)
+        # Linear function 1: 784 --> 500
+        self.fc1 = nn.Linear(1*28*28, 500)
+
+        # Linear function 2: 500 --> 150
+        self.fc2 = nn.Linear(500, 150)
+
+        # Linear function 3: 150 --> 10
+        self.fc3 = nn.Linear(150, 10)
+
+    def forward(self, input):
+
+        # reshape input
+        x = input.view([-1, 1*28*28])
+
+        # Linear function 1
         x = self.fc1(x)
+
+        # Non-linearity 1
         x = F.relu(x)
-        x = self.dropout2(x)
+
+        # Linear function 2
         x = self.fc2(x)
-        output = F.log_softmax(x, dim=1)
-        return output
+
+        # Non-linearity 2
+        x = F.relu(x)
+
+        # Linear function 3
+        out = self.fc3(x)
+
+        out = F.log_softmax(out, dim=1)
+
+        return out
 
 
-def train(args, model, device, train_loader, optimizer, epoch):
+# 连接到GPU
+model = ANNModel().to(DEVICE)
+# 创建优化器并传入参数
+#learning_rate = 0.02
+#optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+optimizer = optim.Adam(model.parameters())
+
+
+def train(model, device, train_loader, optimizer, epoch):
+    # 将模型转为train模式
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
+        # 重置优化器梯度
         optimizer.zero_grad()
         output = model(data)
         loss = F.nll_loss(output, target)
+        # backward传播
         loss.backward()
+        # 更新参数
         optimizer.step()
-        if batch_idx % args.log_interval == 0:
+        if((batch_idx) % 30) == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
-            if args.dry_run:
-                break
 
 
 def test(model, device, test_loader):
+    # 将模型转为test模式
     model.eval()
+    # loss
     test_loss = 0
+    # 正确数
     correct = 0
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            # sum up batch loss
             test_loss += F.nll_loss(output, target, reduction='sum').item()
-            # get the index of the max log-probability
-            pred = output.argmax(dim=1, keepdim=True)
+            pred = output.max(1, keepdim=True)[1]
             correct += pred.eq(target.view_as(pred)).sum().item()
 
     test_loss /= len(test_loader.dataset)
-
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
 
-def main():
-    # Training settings
-    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                        help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                        help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=14, metavar='N',
-                        help='number of epochs to train (default: 14)')
-    parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
-                        help='learning rate (default: 1.0)')
-    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
-                        help='Learning rate step gamma (default: 0.7)')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
-    parser.add_argument('--no-mps', action='store_true', default=False,
-                        help='disables macOS GPU training')
-    parser.add_argument('--dry-run', action='store_true', default=False,
-                        help='quickly check a single pass')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
-                        help='random seed (default: 1)')
-    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                        help='how many batches to wait before logging training status')
-    parser.add_argument('--save-model', action='store_true', default=False,
-                        help='For Saving the current Model')
-    args = parser.parse_args()
-    use_cuda = not args.no_cuda and torch.cuda.is_available()
-    use_mps = not args.no_mps and torch.backends.mps.is_available()
-
-    torch.manual_seed(args.seed)
-
-    if use_cuda:
-        device = torch.device("cuda")
-    elif use_mps:
-        device = torch.device("mps")
-    else:
-        device = torch.device("cpu")
-
-    train_kwargs = {'batch_size': args.batch_size}
-    test_kwargs = {'batch_size': args.test_batch_size}
-    if use_cuda:
-        cuda_kwargs = {'num_workers': 1,
-                       'pin_memory': True,
-                       'shuffle': True}
-        train_kwargs.update(cuda_kwargs)
-        test_kwargs.update(cuda_kwargs)
-
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,))
-    ])
-    dataset1 = datasets.MNIST('../data', train=True, download=True,
-                              transform=transform)
-    dataset2 = datasets.MNIST('../data', train=False,
-                              transform=transform)
-    train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
-    test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
-
-    model = Net().to(device)
-    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
-
-    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
-    for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
-        test(model, device, test_loader)
-        scheduler.step()
-
-    if args.save_model:
-        torch.save(model.state_dict(), "mnist_cnn.pt")
-
-
-if __name__ == '__main__':
-    main()
+# 进行两次训练，第二次训练取第一次训练得出的hyperparameters
+for epoch in range(1, EPOCHS+1):
+    train(model, DEVICE, train_loader, optimizer, epoch)
+    test(model, DEVICE, test_loader)
